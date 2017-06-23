d95098fae78081723334ea099fd353d3e2e835e9
Ada Solcan <adasolcan@yahoo.com>
2017-06-23 17:17:41 +0100

    Add tests and add a default message

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -5,15 +5,20 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/{name}/hello")
 public class GreetingController {
 
     private static final String helloTemplate = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping("/{name}/hello")
     public Greeting readGreeting(@PathVariable String name) {
         return new Greeting(counter.incrementAndGet(),
                 String.format(helloTemplate, name));
+    }
+
+    @RequestMapping("/hello")
+    public Greeting readDefaultGreeting() {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(helloTemplate, "hello"));
     }
 }
